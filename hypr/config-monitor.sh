7b4d22e767bb11b931552a81f0e9fdb933f20cf6
Netanel Friedman <netanel.friedman6@gmail.com>
2025-06-28 00:13:18 +0300

    Change monitor configuration to be the same as I'm used to in river/dwm.

---
+++ hypr/config-monitor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+total=$(hyprctl monitors | awk '/Monitor/ {count++} END {print count}')
+
+if [ "$total" -eq 2 ]
+then
+   hyprctl keyword monitor "eDP-1,disable"
+else
+   hyprctl keyword monitor "eDP-1,preferred,auto,1"
+fi
