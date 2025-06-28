88c04b9e3ffc488866e7c2fe5a5184772d6bf0cd
Netanel Friedman <netanel.friedman6@gmail.com>
2025-06-28 19:45:20 +0300

    Add hyprsunset for better colors on my laptop monitor

--- hypr/config-monitor.sh
+++ hypr/config-monitor.sh
@@ -5,6 +5,8 @@ total=$(hyprctl monitors | awk '/Monitor/ {count++} END {print count}')
 if [ "$total" -eq 2 ]
 then
    hyprctl keyword monitor "eDP-1,disable"
+   hyprctl hyprsunset identity
 else
    hyprctl keyword monitor "eDP-1,preferred,auto,1"
+   hyprctl hyprsunset temperature 5600
 fi
