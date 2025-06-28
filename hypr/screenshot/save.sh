2e56106ccf45b08bef496193d682478a0ad43a30
Netanel Friedman <netanel.friedman6@gmail.com>
2025-06-28 18:58:57 +0300

    Use a different clipboard and add clipboard history

--- hypr/screenshot/save.sh
+++ hypr/screenshot/save.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tee $HOME/Pictures/Screenshots/$(date +"Screenshot-from-%Y-%m-%d-%H-%M-%S").png | waycopy -t image/png
+tee $HOME/Pictures/Screenshots/$(date +"Screenshot-from-%Y-%m-%d-%H-%M-%S").png | wl-copy -t image/png
