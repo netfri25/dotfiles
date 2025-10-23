3f2bc3143479cd91ba7ee7683861754fcc5e5776
Netanel Friedman <netanel.friedman6@gmail.com>
2025-10-23 19:11:49 +0300

    Set neovide refresh rate to 144hz

--- nvim/lua/user/neovide.lua
+++ nvim/lua/user/neovide.lua
@@ -1,7 +1,7 @@
 if not vim.g['neovide'] then return end
 
 -- vim.g.neovide_cursor_vfx_mode = 'ripple'
-vim.g.neovide_refresh_rate = 120
+vim.g.neovide_refresh_rate = 144
 vim.g.neovide_no_idle = false
 -- vim.g.neovide_refresh_rate_idle = 5
 vim.g.neovide_fullscreen = false
