74f62e29ec326ec8159b76a14d68fd401c5d64dd
Netanel Friedman <netanel.friedman6@gmail.com>
2025-10-30 14:16:59 +0200

    Move the toki script out of the river directory

---
+++ toki.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: $0 <from|to>"
+    exit 1
+fi
+
+# keep a copy of the clipboard
+prev=$(wl-paste -n)
+
+wl-paste -np | sitelen-ucsur "$1" | wl-copy -n
+
+wtype -M ctrl -k v
+
+# wait for the paste to register
+sleep 0.1s
+
+# restore clipboard
+wl-copy -n "$prev"
