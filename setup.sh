92d254d6bfec6cf4e489806d084b461d02ab03a8
Netanel Friedman <netanel.friedman6@gmail.com>
2026-02-20 14:21:52 +0200

    Fix accidental typo

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ set -xe
 
 CONFIG_PATH="$HOME/.config"
 mkdir -p $CONFIG_PATH
-setup
+
 # NOTE: not using ls on purpose, in case there will be some other configs that require different locations
 CONFIGS=(
     foot
