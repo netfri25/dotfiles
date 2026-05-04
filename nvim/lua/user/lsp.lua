6e5198b5ec1862f3293c5e20a6fb5e06dd372ac7
Netanel Friedman <netanel.friedman6@gmail.com>
2026-05-04 21:52:09 +0300

    Remove unused junk from lsp config in neovim

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -1,37 +1,39 @@
-local servers = {
-    'rust-analyzer',
-    'pyright',
-    'clangd',
-    'lua_ls',
-    'ruff',
-    'hls',
-}
-
-local enabled = false
-
-vim.api.nvim_create_user_command("Lsp", function(args)
-    enabled = not enabled
-    vim.lsp.enable(servers, enabled)
-end, {})
-
 -- Hide all semantic highlights
+-- since I sometimes program without lsp, I like that there's a consistency in highlights
 for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-  vim.api.nvim_set_hl(0, group, {})
+    vim.api.nvim_set_hl(0, group, {})
 end
 
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('my.lsp', {}),
     callback = function(args)
-        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { silent = true, desc = 'Show diagnostics', buffer = args.buf })
-        vim.keymap.set('n', '<leader>q', function() vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.ERROR }) end, { silent = true, desc = 'Show loclist', buffer = args.buf })
-        vim.keymap.set('n', '<leader>Q', function() vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.WARN }) end, { silent = true, desc = 'Show loclist', buffer = args.buf })
-        vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, { silent = true, desc = 'Goto declaration', buffer = args.buf })
-        vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, { silent = true, desc = 'Goto definition', buffer = args.buf })
-        vim.keymap.set('n', 'K', function() vim.lsp.buf.hover({ border = "rounded" }) end, { silent = true, desc = 'Show docs', buffer = args.buf })
-        vim.keymap.set('n', '<c-s>', function() vim.lsp.buf.signature_help({ border = "rounded" }) end, { silent = true, desc = 'Show signature help', buffer = args.buf })
-        vim.keymap.set('n', 'gt', function() vim.lsp.buf.type_definition({ border = "rounded" }) end, { silent = true, desc = 'Show type definition', buffer = args.buf })
-        vim.keymap.set('n', 'gra', function() vim.lsp.buf.code_action({ border = "rounded" }) end, { silent = true, desc = 'Code actions', buffer = args.buf })
-        vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format({ async = true }) end, { silent = true, desc = 'Format buffer', buffer = args.buf })
+        vim.keymap.set(
+            'n',
+            '<leader>q',
+            function() vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.ERROR }) end,
+            { silent = true, desc = 'show quickfix list for errors', buffer = args.buf }
+        )
+
+        vim.keymap.set(
+            'n',
+            '<leader>Q',
+            function() vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.WARN }) end,
+            { silent = true, desc = 'show quickfix list for warnings and errors', buffer = args.buf }
+        )
+
+        vim.keymap.set(
+            'n',
+            '<leader>e',
+            vim.diagnostic.open_float,
+            { silent = true, desc = 'show diagnostics under cursor', buffer = args.buf }
+        )
+
+        vim.keymap.set(
+            'n',
+            '<leader>f',
+            function() vim.lsp.buf.format({ async = true }) end,
+            { silent = true, desc = 'format buffer', buffer = args.buf }
+        )
     end
 })
 
@@ -50,4 +52,3 @@ vim.diagnostic.config({
         prefix = '',
     },
 })
-
