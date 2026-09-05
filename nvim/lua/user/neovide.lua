a97a74bf389e0d09fe8e0d1e4c5adbfa04facb26
Netanel Friedman <netanel.friedman6@gmail.com>
2026-09-05 14:56:44 +0300

    Disable progress bar in neovide

--- nvim/lua/user/neovide.lua
+++ nvim/lua/user/neovide.lua
@@ -7,6 +7,7 @@ vim.g.neovide_cursor_trail_size = 0.8
 vim.g.neovide_cursor_animation_length = 0.1
 vim.g.neovide_cursor_antialiasing = true
 vim.g.neovide_profiler = false
+vim.g.neovide_progress_bar_enabled = false
 
 
 local resize_gui_font = function(delta)
